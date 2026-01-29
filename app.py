ae1d0e1ec2b0687d5af09e85c9a6d6a0acf098ba
Trần Văn Tuấn <tuantran6304@gmail.com>
2026-01-29 23:18:09 +0700

    fix deploy 2

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def serve_static(filename):
     return send_from_directory('.', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
